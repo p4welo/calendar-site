e9fa48688047cfaf4e6b7fc3684ebb1f083f93e4
p4welo <p4welo@gmail.com>
2019-02-05 08:53:48 +0100

    env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
+let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || 'development';
+
+console.log(`Using environment config: '${activeEnv}'`);
+
+require('dotenv').config({
+  path: `.env.${activeEnv}`,
+});
+
 module.exports = {
   siteMetadata: {
+    siteUrl: `http://www.tanecznykalendarz.pl`,
     title: `Taneczny kalendarz`,
     description: `Kalendarz taneczny to wszystkie turnieje tańca w jednym miejscu! Zgłoś swój turniej ZA DARMO i dotrzyj do nowych odbiorców.`,
     author: `Paweł Radomski`,
@@ -15,8 +24,8 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     // `gatsby-plugin-offline`,
-    // `gatsby-plugin-manifest`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -34,8 +43,20 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: "pages",
+        name: 'pages',
       },
     },
+    {
+      resolve: `gatsby-plugin-fullstory`,
+      options: {
+        fs_org: process.env.FULLSTORY_ORG_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+      },
+    }
   ],
-}
+};
